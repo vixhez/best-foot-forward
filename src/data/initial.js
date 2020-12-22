af277ba19831dc3be492e611ee6d17ce6cdffc4e
Victoria Herod <vicky_herod@live.com>
2020-12-22 13:10:40 +0000

    Initial states updated to provide for updated settings layout

---
+++ src/data/initial.js
@@ -0,0 +1,13 @@
+const initial = {
+    playerNames: [],
+    amountPlayers: 0,
+    team1Name: 'Team Uno',
+    team2Name: 'Team Dos',
+    team1Kit: 'purple',
+    team2Kit: 'green',
+    team1Skills: 5,
+    team2Skills: 5,
+    info: false,
+}
+
+export default initial;
