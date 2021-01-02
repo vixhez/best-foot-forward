d66996f16cd3a11e90578648627a9901c345f007
Victoria Herod <vicky_herod@live.com>
2021-01-02 19:18:30 +0000

    Further match page styling, typography and positioning of team players on pitch

--- src/components/Match/Match.js
+++ src/components/Match/Match.js
@@ -6,32 +6,41 @@ import MatchTeam2 from '../MatchTeam/MatchTeam2';
 import team1Crowd from '../../assets/team1Crowd.png';
 import team2Crowd from '../../assets/team2Crowd.png';
 
-const Match = ({ team1Kit, team2Kit, team1Design, team2Design, team1Banner, team2Banner }) => (
+const Match = ({ team1Kit, team2Kit, team1Design, team2Design, team1Banner, team2Banner, team1Name, team2Name }) => (
     <div className='matchParent'>
         <div className='headerParent'>
-            <h1><Team1Header /> vs <Team2Header /></h1>
+            <h2>{team1Name} <span>vs</span> {team2Name}</h2>
         </div>
-        <div className='team1Parent'>
-            <div
-                className='team1Crowd'
-                style={{ backgroundImage: `url(${team1Design})` }}>
-                <img src={team1Crowd} alt='Cartoon crowd' />
-                <p className='teamBanner'>
-                    {team1Banner}
-                </p>
+        <div className='pitch'>
+            <div className='team1Parent'>
+                <div
+                    className='team1Crowd'
+                    style={{ backgroundImage: `url(${team1Design})` }}>
+                    <div className='team1CrowdImage'>
+                        <img src={team1Crowd} alt='Cartoon crowd' />
+                    </div>
+                    <p className='teamBanner'>
+                        {team1Banner}
+                    </p>
+                </div>
+                <div className='team1Match'>
+                    <MatchTeam1 />
+                </div>
             </div>
-            <MatchTeam1 />
-        </div>
-        <div className='team2Parent'>
-            <MatchTeam2 />
-            <div
-                className='team2Crowd'
-                style={{ backgroundImage: `url(${team2Design})` }}>
-                <img src={team2Crowd} alt='Cartoon crowd' />
-                <p className='teamBanner'>
-                    {team2Banner}
-                </p>
-
+            <div className='team2Parent'>
+                <div className='team2Match'>
+                    <MatchTeam2 />
+                </div>
+                <div
+                    className='team2Crowd'
+                    style={{ backgroundImage: `url(${team2Design})` }}>
+                    <div className='team2CrowdImage'>
+                        <img src={team2Crowd} alt='Cartoon crowd' />
+                    </div>
+                    <p className='teamBanner'>
+                        {team2Banner}
+                    </p>
+                </div>
             </div>
         </div>
     </div>
