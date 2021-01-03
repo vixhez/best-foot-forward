32c32e244f18dcb04275ecda145306fd3e61427d
Victoria Herod <vicky_herod@live.com>
2021-01-03 16:36:27 +0000

    Instructions sidebars complete for both landing and players page

--- src/components/SettingsInstructionsAndAbout/SettingsInstructionsAndAbout.js
+++ src/components/SettingsInstructionsAndAbout/SettingsInstructionsAndAbout.js
@@ -1,18 +1,34 @@
 const SettingsInstructionsAndAbout = () => (
     <div className='sidebarSettingsAndAbout'>
-        <div className='sidebarSettings'>
+        <div className='sidebarWelcome'>
             <h6>
                 Welcome!
             </h6>
             <p>
-
+                Best Foot Forward allows you to create, customise and coordinate your own football teams, all while feeling a bit like you are floating in the serene backwaters of space.
             </p>
-
         </div>
-        <div className='sidebarAbout'>
 
-
+        <div className='sidebarSettings'>
+            <h6>
+                Guidance
+            </h6>
             <p>
+                Begin by choosing the number of players you would like on each team.
+                You can then proceed to innovating your very own team name.
+                Then, step into the sports boutique to try on different combinations of team kits and designs. Do this by clicking the clothing and pattern icons to mix and match.
+                Next, think up a banner message to be held by the team's supporters during the match.
+                Do the same for the second team and then click 'Save Settings'. 
+            </p>
+        </div>
+
+        <div className='sidebarAbout'>
+        <h6>
+                About
+            </h6>
+            <p>
+                This app is the fruit of an assigned technical challenge as part of Develop Me's Coding Fellowship Bootcamp. You are very welcome to check out the code <a href='https://github.com/vixhez/best-foot-forward' target='_blank'
+                rel='noreferrer'>here</a>.
 
             </p>
         </div>
